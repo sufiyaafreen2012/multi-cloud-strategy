9bf1fdf31385f8d8e5f144e2319be87145312071
Sufiya Afreen <sufiyaafreen12@gmail.com>
2025-05-15 00:52:45 +0530

    Added React frontend

---
+++ app/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>Multi-Cloud Strategy Frontend</p>
+    </div>
+  );
+}
+
+export default App;
